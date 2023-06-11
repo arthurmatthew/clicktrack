606298154d6d73c3f28eaa20c86201917f0c593a
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-10 19:14:06 -0700

    Add footer to unique file

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,42 @@
+const Footer = () => {
+  return (
+    <footer className="flex shrink grow-0 basis-44 flex-wrap justify-evenly bg-slate-900 px-4 py-12 leading-loose text-purple-100 lg:px-64 xl:px-72 2xl:px-96 [&>*]:px-4">
+      <div className="mt-2 flex flex-col gap-2">
+        <h2 className="text-lg text-purple-300">clicktrack</h2>
+        <ul className="underline">
+          <li>Read the Docs</li>
+          <li>Get Started</li>
+          <li>First Project</li>
+          <li>Exporting</li>
+        </ul>
+      </div>
+      <div className="mt-2 flex flex-col gap-2">
+        <h2 className="text-lg text-purple-300">Support</h2>
+        <ul className="underline">
+          <li>Bug Reports</li>
+          <li>Feature Requests</li>
+        </ul>
+      </div>
+      <div className="mt-2 flex flex-col gap-2">
+        <h2 className="text-lg text-purple-300">Community</h2>
+        <ul className="underline">
+          <li>GitHub</li>
+          <li>Twitter</li>
+          <li>TikTok</li>
+          <li>Patreon</li>
+        </ul>
+      </div>
+      <div className="mt-2 flex flex-col gap-2">
+        <h2 className="text-lg text-purple-300">Placeholder</h2>
+        <ul className="underline">
+          <li>Just</li>
+          <li>A</li>
+          <li>Simple</li>
+          <li>Placeholder</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
