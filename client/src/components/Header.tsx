364261d80993bf9277c7c22958737a037188a760
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-10 19:14:15 -0700

    Add header to unique file

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-10 shrink grow-0 basis-auto p-5 backdrop-blur-md">
+      <a href="/" className="text-4xl text-purple-500">
+        clicktrack
+      </a>
+    </header>
+  );
+};
+
+export default Header;
