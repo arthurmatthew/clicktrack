9e93814eac538c571b8bb9aed6c25f51af1ba706
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-11 00:52:34 -0700

    Placeholder image

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,24 +1,26 @@
 import { motion } from 'framer-motion';
 import './index.css';
 
+import placeholder from '../../assets/placeholder.jpg';
+
 const Index = () => {
   return (
     <>
       <section className="flex flex-col items-center justify-center px-2">
         <div className="mx-auto flex max-w-5xl flex-col justify-center py-20">
           <motion.h1
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
             id="title"
-            className="bg-gradient-to-r from-purple-400 via-blue-500 to-purple-700 bg-clip-text text-center text-5xl font-black text-transparent sm:text-7xl"
+            className="bg-gradient-to-r from-purple-400 via-violet-400 to-purple-700 bg-clip-text text-center text-5xl font-black text-transparent sm:text-7xl"
           >
             The only metronome you'll ever need.
           </motion.h1>
           <motion.p
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, delay: 0.1 }}
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.1 }}
             className="lora mx-2 mt-10 text-center text-base text-purple-200 sm:text-lg"
           >
             Seriously. <i className="inter not-italic">clicktrack</i> is a{' '}
@@ -30,9 +32,9 @@ const Index = () => {
           </motion.p>
           <div className="mt-10 flex flex-wrap justify-center gap-2">
             <motion.a
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
+              initial={{ opacity: 0, x: -200 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.3 }}
               href="/app"
               target="_blank"
             >
@@ -46,9 +48,9 @@ const Index = () => {
               </motion.button>
             </motion.a>
             <motion.a
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.5, delay: 0.3 }}
+              initial={{ opacity: 0, x: -200 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.2 }}
             >
               <motion.button
                 whileTap={{ scale: 0.9 }}
@@ -62,7 +64,22 @@ const Index = () => {
           </div>
         </div>
       </section>
-      <section className="h-screen"></section>
+      <section className="my-20 flex flex-col px-2">
+        <div className="mx-auto flex w-full max-w-6xl flex-col">
+          <div className="aspect-video w-full overflow-hidden rounded-lg rounded-b-none bg-black">
+            <img src={placeholder} className="object-cover" />
+          </div>
+        </div>
+        <div className="mx-auto flex w-full max-w-6xl flex-col">
+          <p className="w-full rounded-lg rounded-t-none bg-purple-300 p-5 text-xl">
+            This is an awesome placeholder image. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Cupiditate itaque officiis
+            dignissimos nam nemo perferendis voluptatibus libero omnis vitae.
+            Quo quas recusandae tenetur fuga harum, aut nobis quod temporibus
+            debitis.
+          </p>
+        </div>
+      </section>
       <section className="flex flex-col items-center p-10 text-purple-200">
         <p className="text-center">
           Thank you for choosing clicktrack. This project was made by Matthew
