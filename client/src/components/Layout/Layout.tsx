208f8e8eab8bf7f608e723bd4617d2e50107a7fa
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-10 19:14:25 -0700

    Move header and footer to own file

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,15 +1,13 @@
+import Footer from '../Footer';
+import Header from '../Header';
 import './Layout.css';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-slate-950" id="background">
-      <header className="sticky top-0 p-5 backdrop-blur-md">
-        <h1 className="bg-gradient-to-tr from-purple-400 to-purple-600 bg-clip-text text-4xl text-transparent">
-          clicktrack
-        </h1>
-      </header>
-      <main>{children}</main>
-      <footer></footer>
+    <div className="bg-slate-950 flex min-h-screen flex-col" id="background">
+      <Header />
+      <main className="relative flex-1">{children}</main>
+      <Footer />
     </div>
   );
 };
