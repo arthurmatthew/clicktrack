0dd615a6905bca60a1da09bf62667e010d35911e
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-11 23:22:31 -0700

    Create server for auth with jwt

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,36 @@
-// import 'dotenv/config';
-
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
+import db from './configs/db.config';
+
+import users from './routes/users.route';
+import server from './configs/server.config';
+
 const app = express();
+const databaseUrl = db.url;
+const port = server.port;
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (_req, res) => {
-  res.send('Hello World');
-});
+// Routes
+app.use('/users', users);
 
-app.listen(3000, () => {
-  console.log('Listening on 3000');
-});
+mongoose
+  .connect(databaseUrl as string)
+  .then((_res) => {
+    app.listen(port, () => {
+      console.log('Listening on ' + port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// console.log((databaseUrl as string) + mongoose + ' shut up typescript!');
+// app.listen(port, () => {
+//   console.log('Listening without DB on ' + port);
+// });
