ed4e27dc702904948cf2d69970acfaad4f6d7d6d
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-11 20:02:43 -0700

    Add gradient to match header

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex shrink grow-0 basis-44 flex-wrap justify-evenly bg-slate-900 px-4 py-12 leading-loose text-purple-100 lg:px-64 xl:px-72 2xl:px-96 [&>*]:px-4">
+    <footer className="to-slate-950 bg-gradient-radial flex shrink grow-0 basis-44 flex-wrap justify-evenly from-slate-800 bg-[length:100%_200%] bg-[100%_100%] px-4 py-12 leading-loose text-purple-100 lg:px-64 xl:px-72 2xl:px-96 [&>*]:px-4">
       <div className="mt-2 flex flex-col gap-2">
         <h2 className="text-lg text-purple-300">clicktrack</h2>
         <ul className="underline">
