699b948b3b74923be17e71bc4995de303f1a3a55
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-13 21:46:29 -0700

    Update header to use react router links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="bg-gradient-radial from-slate-950 sticky top-0 z-10 shrink grow-0 basis-auto to-slate-800 bg-[length:100%_200%] bg-[100%_100%] p-5">
-      <a href="/" className="text-4xl text-purple-500">
+      <Link to="/" className="text-4xl text-purple-500">
         clicktrack
-      </a>
+      </Link>
     </header>
   );
 };
