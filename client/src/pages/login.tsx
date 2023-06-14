be01719ca1dd670c7c731deca1ebb4543be9e680
Matthew Arthur <89240346+arthurmatthew@users.noreply.github.com>
2023-06-13 23:49:43 -0700

    Remove useEffect used for testing

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -24,10 +24,6 @@ const Login = () => {
     console.log(json.message);
   };
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
