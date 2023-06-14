94df8f8fc16c54979cd2ce1d63ef07a0bfaf7f45
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-13 21:46:39 -0700

    Update layout to support error page

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
 import './Layout.css';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="bg-slate-950 flex min-h-screen flex-col" id="background">
       <Header />
-      <main className="relative flex-1">{children}</main>
+      <main className="relative flex-1">{children ?? <Outlet />}</main>
       <Footer />
     </div>
   );
