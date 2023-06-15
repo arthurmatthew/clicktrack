1816749ece4e6288d401b4003b832630849f54d2
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-14 20:59:53 -0700

    Update layout

--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -1,41 +1,28 @@
 const Footer = () => {
   return (
-    <footer className="to-slate-950 bg-gradient-radial flex shrink grow-0 basis-44 flex-wrap justify-evenly from-slate-800 bg-[length:100%_200%] bg-[100%_100%] px-4 py-12 leading-loose text-purple-100 lg:px-64 xl:px-72 2xl:px-96 [&>*]:px-4">
-      <div className="mt-2 flex flex-col gap-2">
-        <h2 className="text-lg text-purple-300">clicktrack</h2>
-        <ul className="underline">
-          <li>Read the Docs</li>
-          <li>Get Started</li>
-          <li>First Project</li>
-          <li>Exporting</li>
-        </ul>
-      </div>
-      <div className="mt-2 flex flex-col gap-2">
-        <h2 className="text-lg text-purple-300">Support</h2>
-        <ul className="underline">
-          <li>Bug Reports</li>
-          <li>Feature Requests</li>
-        </ul>
-      </div>
-      <div className="mt-2 flex flex-col gap-2">
-        <h2 className="text-lg text-purple-300">Community</h2>
-        <ul className="underline">
-          <li>GitHub</li>
-          <li>Twitter</li>
-          <li>TikTok</li>
-          <li>Patreon</li>
-        </ul>
-      </div>
-      <div className="mt-2 flex flex-col gap-2">
-        <h2 className="text-lg text-purple-300">Placeholder</h2>
-        <ul className="underline">
-          <li>Just</li>
-          <li>A</li>
-          <li>Simple</li>
-          <li>Placeholder</li>
-        </ul>
+    <footer className="to-slate-950 bg-gradient-radial flex basis-44 flex-col justify-center from-slate-800 bg-[length:100%_200%] bg-[100%_100%] py-10 px-4 text-purple-100">
+      <div className="mx-auto grid w-full max-w-6xl grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-4">
+        <FooterSection />
+        <FooterSection />
+        <FooterSection />
+        <FooterSection />
       </div>
     </footer>
+  );
+};
+
+const FooterSection = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h3 className="text-xl font-semibold text-purple-300">Placeholder</h3>
+      <div className="my-5 h-px w-1/2 bg-gradient-to-r from-transparent via-slate-600 to-transparent"></div>
+      <ul className="leading-loose text-slate-200">
+        <li>Placeholder</li>
+        <li>Placeholder</li>
+        <li>Placeholder</li>
+        <li>Placeholder</li>
+      </ul>
+    </div>
   );
 };
 
