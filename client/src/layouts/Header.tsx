d92367c650306fda61fad939b32bb004e5e8364f
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-14 20:59:43 -0700

    Update gradient to match footer

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-gradient-radial from-slate-950 sticky top-0 z-10 shrink grow-0 basis-auto to-slate-800 bg-[length:100%_200%] bg-[100%_100%] p-5">
+    <header className="bg-gradient-radial to-slate-950 sticky top-0 z-10 shrink grow-0 basis-auto from-slate-800 bg-[length:100%_200%] bg-[100%_0] p-5">
       <Link to="/" className="text-4xl text-purple-500">
         clicktrack
       </Link>
