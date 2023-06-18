9263fc24601bb48c3a5fc4cf0da6ff10cb2f8cab
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-18 13:03:15 -0700

    Sticky state hook

---
+++ client/src/hooks/useStickyState.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+type SaveableData = string | number | object | boolean | undefined | null;
+
+export default function useStickyState<T extends SaveableData>(
+  defaultValue: T,
+  key: string
+): [value: T, setValue: Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState(() => {
+    const v = localStorage.getItem(key);
+    if (v === null) {
+      return defaultValue;
+    }
+    try {
+      return JSON.parse(v);
+    } catch (e) {
+      console.error('Could not parse data');
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
