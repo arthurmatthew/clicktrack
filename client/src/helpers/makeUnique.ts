bf6f80302234c5a4ca185c2d765b75001b5fdeb4
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-19 11:58:51 -0700

    Add makeUnique helper

---
+++ client/src/helpers/makeUnique.ts
@@ -0,0 +1,36 @@
+import { Section } from '../pages/app/view-projects';
+
+export default function makeUnique(
+  name: string,
+  newName: string,
+  prev: Section[]
+) {
+  let trials = 0;
+  let uniqueName = newName;
+
+  // console.log(' ---- BEGINNING TEST ---- ');
+  // console.log('Always Add Suffix: ', settings?.alwaysAddSuffix);
+  // console.log('.....');
+  // console.log('Testing for name: ', uniqueName);
+  // console.log('---- TRIALS ----');
+  while (true) {
+    // console.log('Trial ', trials);
+    const exist = [
+      ...prev.filter((metronome) => metronome.name != name),
+      {
+        ...prev.filter((metronome) => metronome.name == name)[0],
+        name: uniqueName,
+      },
+    ].filter((metronome) => metronome.name == uniqueName).length;
+    // console.log(exist, ' ', uniqueName, ' exist already.');
+    if (exist <= 1) {
+      uniqueName = trials == 0 ? newName : `${newName} (#${trials})`;
+      // console.log(uniqueName, ' is available.');
+      break;
+    }
+    trials++;
+    uniqueName = `${newName} (#${trials})`;
+  }
+
+  return uniqueName;
+}
