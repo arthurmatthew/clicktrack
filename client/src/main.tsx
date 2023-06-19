eee4f4b6fced5f50b8d0a047ca4132d2b588465b
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-19 00:14:57 -0700

    Moved routes out to own files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,38 +1,22 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css';
 
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Route,
   RouterProvider,
 } from 'react-router-dom';
 
-const NotFound = lazy(() => import('./components/routes/NotFound'));
-const App = lazy(() => import('./pages/app'));
-const Login = lazy(() => import('./pages/login'));
-const Register = lazy(() => import('./pages/register'));
-
-import Layout from './layouts/Layout';
-import Index from './pages/index';
+import AppRoute from './routes/app.route';
+import IndexRoute from './routes/index.route';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      path="/"
-      element={<Layout />}
-      errorElement={
-        <Layout>
-          <NotFound />
-        </Layout>
-      }
-    >
-      <Route path="/" element={<Index />} />
-      <Route path="/app" element={<App />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Route>
+    <>
+      {IndexRoute}
+      {AppRoute}
+    </>
   )
 );
 
