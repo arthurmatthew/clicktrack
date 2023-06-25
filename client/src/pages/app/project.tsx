918e2fc10cb10ae7297e2f7904ee9d88b3ce9445
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-24 19:24:57 -0700

    Foundation for metronome

---
+++ client/src/pages/app/project.tsx
@@ -0,0 +1,18 @@
+import { useParams } from 'react-router-dom';
+import { Section } from './view-projects';
+
+const Project = () => {
+  const { id } = useParams();
+  const decoded = decodeURIComponent(id as string);
+
+  const getMetronome = (name: string) => {
+    const metronomes = JSON.parse(
+      localStorage.getItem('metronomes') as string
+    ) as Section[];
+    return metronomes.filter((x) => x.name == name)[0];
+  };
+
+  return JSON.stringify(getMetronome(decoded));
+};
+
+export default Project;
