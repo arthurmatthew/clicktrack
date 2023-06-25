63c0a659dedaff80989b84a026bd262e4baa95c9
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-24 19:23:47 -0700

    Make smaller

--- client/src/components/app/CreateSection.tsx
+++ client/src/components/app/CreateSection.tsx
@@ -10,14 +10,15 @@ export const CreateSection = ({
   return (
     <div
       onClick={add}
-      className="w-full rounded-md border-2 border-dashed border-slate-300 bg-slate-200/50 p-4 hover:bg-slate-200 dark:border-slate-700 dark:bg-slate-800/50 dark:hover:bg-slate-800"
+      className="w-full rounded-md border-2 border-dashed border-slate-300 bg-slate-200/50 px-3 py-2 hover:bg-slate-200 dark:border-slate-700 dark:bg-slate-800/50 dark:hover:bg-slate-800"
     >
-      <div className="flex items-center justify-between text-slate-900 dark:text-slate-100">
-        <h1 className="text-3xl font-semibold">
-          <i className={`bi-${icon} mr-3 text-slate-600 dark:text-slate-400`} />
+      <div className="text-slate-900 dark:text-slate-100">
+        <h1 className="flex items-center text-xl font-semibold">
+          <i
+            className={`bi-${icon} mr-3 text-2xl text-slate-600 dark:text-slate-400`}
+          />
           {children}
         </h1>
-        <i className="text-2xl text-slate-600 dark:text-slate-400" />
       </div>
     </div>
   );
