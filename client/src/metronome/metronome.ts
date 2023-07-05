c44cfb4cd7d7e995d7ee0e843121b54b8e2d258b
Matthew Arthur <matthewarthur474@gmail.com>
2023-07-04 23:58:35 -0700

    Make metronome a class

--- client/src/metronome/metronome.ts
+++ client/src/metronome/metronome.ts
@@ -1,48 +1,28 @@
-export default interface Metronome {
+export default class Metronome {
   name: string;
   id: string;
   position: number;
   permanant: boolean;
   data: Data;
-  opened?: boolean;
+  opened: boolean;
+  constructor(options?: Partial<Metronome>) {
+    this.name = options?.name || 'Default Metronome';
+    this.id = options?.id || 'default';
+    this.position = options?.position || -1;
+    this.permanant = options?.permanant || false;
+    this.data = options?.data || new Data({});
+    this.opened = options?.opened || false;
+  }
 }
 
 export class Data {
+  bpm: number;
+
   note: [note: string, octave: number];
   noteDuration: number;
-  constructor(options: Partial<Data>) {
-    this.note = options.note || ['C', 5];
-    this.noteDuration = 1;
-  }
-
-  /**
-   * Get frequency based on note property
-   */
-  get frequency() {
-    const calcOct = (zeroFreq: number, octave: number) => {
-      if (octave === 0) return zeroFreq;
-      return zeroFreq * Math.pow(2, octave);
-    };
-    const note = this.note[0].toUpperCase();
-    const octave = this.note[1];
-    switch (note) {
-      case 'C':
-        return calcOct(16.35, octave);
-      case 'D':
-        return calcOct(18.35, octave);
-      case 'E':
-        return calcOct(20.6, octave);
-      case 'F':
-        return calcOct(21.83, octave);
-      case 'G':
-        return calcOct(24.5, octave);
-      case 'A':
-        return calcOct(27.5, octave);
-      case 'B':
-        return calcOct(30.87, octave);
-      default:
-        console.error(`This note doesn't exist! (${NodeIterator})`);
-        return 440;
-    }
+  constructor(options?: Partial<Data>) {
+    this.bpm = options?.bpm || 120;
+    this.note = options?.note || ['C', 5];
+    this.noteDuration = options?.noteDuration || 1;
   }
 }
