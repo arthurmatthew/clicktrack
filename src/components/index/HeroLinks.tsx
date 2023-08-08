d56cc4b94463ab47deb8a2ec04fcdd4cebcb180f
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-07 21:48:28 -0700

    Animation delay

--- src/components/index/HeroLinks.tsx
+++ src/components/index/HeroLinks.tsx
@@ -3,39 +3,45 @@ import { Link } from 'react-router-dom';
 
 export const HeroLinks = () => {
   return (
-    <div className="mx-2 mt-10 flex flex-col flex-wrap items-center justify-center gap-2 sm:flex-row">
-      <Link to="/app">
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.2 }}
-        >
-          <motion.button
-            whileTap={{ scale: 0.9 }}
-            whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-            className="border-gradient rounded-sm border-2 border-purple-700 bg-purple-700 px-12 py-3 text-xl text-white"
+    <div className="flex flex-col items-center">
+      <div className="mx-2 mt-10 flex flex-col flex-wrap items-center justify-center gap-2 sm:flex-row">
+        <Link to="/app">
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.2 }}
           >
-            Get started
-          </motion.button>
-        </motion.span>
-      </Link>
-      <Link to="/app">
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.2 }}
-        >
-          <motion.button
-            whileTap={{ scale: 0.9 }}
-            whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-            className="border-gradient rounded-sm border-2 border-purple-500 bg-white px-12 py-3 text-xl dark:border-purple-700 dark:bg-black "
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+              className="border-gradient rounded-sm border-2 border-purple-700 bg-purple-700 px-12 py-3 text-xl text-white"
+            >
+              Get started
+            </motion.button>
+          </motion.span>
+        </Link>
+        <Link to="/app">
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.3 }}
           >
-            Learn more
-          </motion.button>
-        </motion.span>
-      </Link>
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+              className="border-gradient rounded-sm border-2 border-purple-500 bg-white px-12 py-3 text-xl dark:border-purple-700 dark:bg-black "
+            >
+              Learn more
+            </motion.button>
+          </motion.span>
+        </Link>
+      </div>
+      <p className="my-8 rounded-full border-[1px] border-white bg-neutral-800 p-2 px-4 text-sm">
+        Clicktrack now lives at{' '}
+        <b className="font-semibold">www.useclicktrack.com</b>
+      </p>
     </div>
   );
 };
