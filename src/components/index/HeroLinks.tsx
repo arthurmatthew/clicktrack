581425beb30d3913fecb8d95e67f3441b9e7612c
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-07 19:36:40 -0700

    Change styling

--- src/components/index/HeroLinks.tsx
+++ src/components/index/HeroLinks.tsx
@@ -3,7 +3,23 @@ import { Link } from 'react-router-dom';
 
 export const HeroLinks = () => {
   return (
-    <div className="mx-2 mt-10 flex flex-col flex-wrap items-center justify-center gap-2">
+    <div className="mx-2 mt-10 flex flex-col flex-wrap items-center justify-center gap-2 sm:flex-row">
+      <div>
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.2 }}
+        >
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ scale: 1.05 }}
+            transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+            className="border-gradient rounded-sm border-2 border-purple-700 bg-purple-700 px-12 py-3 text-xl text-white"
+          >
+            Get started
+          </motion.button>
+        </motion.span>
+      </div>
       <Link to="/app">
         <motion.span
           initial={{ opacity: 0 }}
@@ -16,7 +32,7 @@ export const HeroLinks = () => {
             transition={{ type: 'spring', stiffness: 400, damping: 17 }}
             className="border-gradient rounded-sm border-2 border-purple-500 bg-white px-12 py-3 text-xl dark:border-purple-700 dark:bg-black "
           >
-            Get started
+            Learn more
           </motion.button>
         </motion.span>
       </Link>
