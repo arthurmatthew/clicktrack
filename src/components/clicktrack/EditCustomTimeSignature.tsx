dd1ad7f6c2e008ff55eede988fef4c85e1462598
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-10 14:47:28 -0700

    Styling changes

--- src/components/clicktrack/EditCustomTimeSignature.tsx
+++ src/components/clicktrack/EditCustomTimeSignature.tsx
@@ -30,39 +30,41 @@ export const EditCustomTimeSignature = ({
     });
 
   return (
-    <div className="row-span-2 flex h-full w-full flex-col items-center justify-center border-neutral-200 bg-white text-4xl dark:border-neutral-900 dark:bg-black">
-      <h2 className="inter text-xs font-normal opacity-50">Beats</h2>
-      <div className="flex items-center gap-4">
+    <div className="row-span-2 flex h-full w-full flex-col items-center justify-center gap-2 border-neutral-200 bg-white text-3xl dark:border-neutral-900 dark:bg-black">
+      <div className="flex items-center gap-2">
         <button
           onClick={increaseNumerator}
           className="flex aspect-square h-7 items-center justify-center rounded-full bg-neutral-900"
         >
-          <i className="bi-plus-lg text-xl leading-[0]" />
+          <i className="bi-plus leading-[0]" />
         </button>
-        <h1 className="w-14 text-center">{metronome.timeSignature[0]}</h1>
+        <h1 className="w-14 text-center leading-[0]">
+          {metronome.timeSignature[0]}
+        </h1>
         <button
           onClick={decreaseNumerator}
           className="flex aspect-square h-7 items-center justify-center rounded-full bg-neutral-900"
         >
-          <i className="bi-dash-lg text-xl leading-[0]" />
+          <i className="bi-dash leading-[0]" />
         </button>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
         <button
           onClick={increaseDenominator}
           className="flex aspect-square h-7 items-center justify-center rounded-full bg-neutral-900"
         >
-          <i className="bi-plus-lg text-xl leading-[0]" />
+          <i className="bi-plus leading-[0]" />
         </button>
-        <h1 className="w-14 text-center">{metronome.timeSignature[1]}</h1>
+        <h1 className="w-14 text-center leading-[0]">
+          {metronome.timeSignature[1]}
+        </h1>
         <button
           onClick={decreaseDenominator}
           className="flex aspect-square h-7 items-center justify-center rounded-full bg-neutral-900"
         >
-          <i className="bi-dash-lg text-xl leading-[0]" />
+          <i className="bi-dash leading-[0]" />
         </button>
       </div>
-      <h2 className="inter text-xs font-normal opacity-50">Value</h2>
     </div>
   );
 };
