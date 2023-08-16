a106c744ee1702ffe2b96a70621394485cc2b11b
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-16 16:02:54 -0700

    Restrict to y axis only

--- src/components/clicktracks/DraggableItem.tsx
+++ src/components/clicktracks/DraggableItem.tsx
@@ -20,29 +20,37 @@ export const DraggableItem = ({
 }: IDraggableItem) => {
   return (
     <Draggable key={clicktrack.id} draggableId={clicktrack.id} index={index}>
-      {(provided) => (
-        <li
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          className="my-1"
-        >
-          <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.3, ease: 'easeOut' }}
+      {(provided) => {
+        const transform = provided.draggableProps.style?.transform;
+        if (transform && provided.draggableProps.style) {
+          const transformY = transform.split(',')[1];
+          provided.draggableProps.style.transform =
+            'translate(0px,' + transformY;
+        }
+        return (
+          <li
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            className="my-1"
           >
-            <ClicktrackListItem
-              handleRemove={() => {
-                handleRemove(clicktrack.id);
-              }}
-              handleNameChange={handleNameChange}
-              handleCopy={handleCopy}
-              clicktrack={clicktrack}
-              dragHandle={provided.dragHandleProps}
-            />
-          </motion.div>
-        </li>
-      )}
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.3, ease: 'easeOut' }}
+            >
+              <ClicktrackListItem
+                handleRemove={() => {
+                  handleRemove(clicktrack.id);
+                }}
+                handleNameChange={handleNameChange}
+                handleCopy={handleCopy}
+                clicktrack={clicktrack}
+                dragHandle={provided.dragHandleProps}
+              />
+            </motion.div>
+          </li>
+        );
+      }}
     </Draggable>
   );
 };
