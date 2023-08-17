0cf2449dafadaedc491377a70073f05dc6493ea9
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-16 17:13:37 -0700

    Try to improve SEO

--- src/components/clicktracks/Heading.tsx
+++ src/components/clicktracks/Heading.tsx
@@ -7,7 +7,11 @@ interface IHeading {
 export const Heading = ({ handleAdd }: IHeading) => {
   return (
     <>
-      <h1 className="text-3xl  ">Your Clicktracks</h1>
+      <h1 className="text-3xl font-semibold">Your Clicktracks</h1>
+      <h2 className="text-2xl opacity-80">
+        View all your clicktracks here. They're saved to your browser
+        automatically.
+      </h2>
       <div className="flex flex-col gap-2">
         <InteractableListItem icon="plus-square" interaction={handleAdd}>
           Create New
