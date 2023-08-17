b8aa0928c25ed865140d7028cdde709c01410b37
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-17 13:37:47 -0700

    Styling

--- src/components/clicktrack/Sequencer.tsx
+++ src/components/clicktrack/Sequencer.tsx
@@ -64,7 +64,7 @@ export const Sequencer = ({
                               (section) =>
                                 section instanceof Repeat && section.infinite
                             ) !== undefined
-                            ? 'opacity-50'
+                            ? 'opacity-30'
                             : ''
                         }
                       >
