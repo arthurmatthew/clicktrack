2013d88deb7ceb3501961c4fb77470c6016f78dd
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-19 13:43:06 -0700

    Styling changes

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -14,7 +14,7 @@ export const Button = ({ children, className, onClick, disabled }: IButton) => {
       whileTap={{ scale: 0.9 }}
       whileHover={{ scale: 1.05 }}
       transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-      className={'rounded-sm px-10 py-2 ' + className}
+      className={'select-none rounded-sm px-10 py-2 ' + className}
     >
       {children}
     </motion.button>
