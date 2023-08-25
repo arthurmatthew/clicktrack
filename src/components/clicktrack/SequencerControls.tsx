c25f72b7179d9f7347643eda3fc3e9a6a677645c
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-25 11:41:29 -0700

    Remove hover effect

--- src/components/clicktrack/SequencerControls.tsx
+++ src/components/clicktrack/SequencerControls.tsx
@@ -11,13 +11,13 @@ export const SequencerControls = ({
     <div className="m-4 flex items-center gap-3">
       <button
         onClick={addMetronome}
-        className="h-full w-1/2 rounded-sm bg-neutral-300 p-4 py-3 duration-100 ease-out hover:w-7/12 dark:bg-neutral-900"
+        className="h-full w-1/2 rounded-sm bg-neutral-300 p-4 py-3 dark:bg-neutral-900"
       >
         Add a Metronome
       </button>
       <button
         onClick={addRepeat}
-        className="h-full w-1/2 rounded-sm bg-neutral-300 p-4 py-3 duration-100 ease-out hover:w-7/12 dark:bg-neutral-900"
+        className="h-full w-1/2 rounded-sm bg-neutral-300 p-4 py-3 dark:bg-neutral-900"
       >
         Add a Repeat
       </button>
