ac6e0d05132bd1f72b4249971243cb6491757ae1
simon <simon.janssen@live.de>
2024-07-27 17:10:42 +0200

    + click click Source + whitenoise click source

--- src/hooks/usePlayClicktrack.ts
+++ src/hooks/usePlayClicktrack.ts
@@ -5,11 +5,14 @@ import { validatePlay } from '../utils/validators/validatePlay';
 import { useNotify } from './useNotify';
 import { Metronome } from '../models/Metronome';
 
+const nulledAudioBuffer = new AudioBuffer({length:1, sampleRate: 44100})
+
 export const usePlayClicktrack = (
   _clicktrack: Clicktrack,
   onClick: () => void
 ) => {
   const audioCtx = useRef<AudioContext | null>(null);
+  const buffers = useRef<{noise:AudioBuffer, click:AudioBuffer}>({noise:nulledAudioBuffer, click:nulledAudioBuffer});
   const { notify } = useNotify();
 
   const interval = useRef<number | null>();
@@ -39,6 +42,20 @@ export const usePlayClicktrack = (
 
   useEffect(() => {
     audioCtx.current = new AudioContext();
+
+    const bufferSize = 2 * audioCtx.current.sampleRate;
+    buffers.current.noise = audioCtx.current.createBuffer(1, bufferSize, audioCtx.current.sampleRate);
+    const output = buffers.current.noise.getChannelData(0);
+    for (let i = 0; i < bufferSize; i++) {
+      output[i] = Math.random() * 2 - 1;
+    }
+
+    void (async () =>
+        buffers.current.click = await fetch('/sounds/click.wav')
+          .then(res => res.arrayBuffer())
+          .then(buffer => audioCtx.current?.decodeAudioData(buffer) ?? nulledAudioBuffer)
+    )();
+
   }, []);
 
   const schedule = (beat: number, time: number) => {
@@ -62,9 +79,10 @@ export const usePlayClicktrack = (
     if (section instanceof Repeat) return;
 
     const noteType = section.timeSignature[1];
-    const oscillator = audioCtx.current.createOscillator();
-    const masterGain = audioCtx.current.createGain();
-    const localGain = audioCtx.current.createGain();
+
+    if (noteType === 8 && beat % 2) return; // Not divisible by 2 means the current beat is not an 8th note
+    if (noteType === 4 && beat % 4) return; // Note divisible by 4 means the current beat is not a 16th note
+    if (noteType === 2 && beat % 8) return;
 
     const calculatedMasterVolume = 1 * (clicktrack.current.data.volume / 100);
     const masterVolume = clicktrack.current.data.muted
@@ -75,10 +93,6 @@ export const usePlayClicktrack = (
 
     const [downbeatIsAccented, backbeatIsAccented] = section.accents;
 
-    if (noteType === 8 && beat % 2) return; // Not divisible by 2 means the current beat is not an 8th note
-    if (noteType === 4 && beat % 4) return; // Note divisible by 4 means the current beat is not a 16th note
-    if (noteType === 2 && beat % 8) return;
-
     const isDownbeat = beat === 0;
     const isBackbeat = beat === 8;
     const isAccentedDownbeat = isDownbeat && downbeatIsAccented;
@@ -86,16 +100,31 @@ export const usePlayClicktrack = (
 
     onClick();
 
+    const oscillator = audioCtx.current.createOscillator();
+    const whiteNoiseSource = audioCtx.current.createBufferSource();
+    whiteNoiseSource.buffer = buffers.current.noise;
+    const clickSource = audioCtx.current.createBufferSource();
+    clickSource.buffer = buffers.current.click;
+
+    const masterGain = audioCtx.current.createGain();
+    const localGain = audioCtx.current.createGain();
+    const relativeGain = audioCtx.current.createGain();
+
     oscillator.frequency.value = 440.0;
     if (isAccentedDownbeat || isAccentedBackbeat) {
       oscillator.frequency.value = 880.0;
     }
 
     masterGain.gain.value = masterVolume;
+    relativeGain.gain.value = 3;
 
     masterGain.connect(audioCtx.current.destination);
     localGain.connect(masterGain);
+    relativeGain.connect(localGain);
+
     oscillator.connect(localGain);
+    whiteNoiseSource.connect(localGain);
+    clickSource.connect(relativeGain);
 
     // Give it a nicer sound by fading out.
     localGain.gain.setValueAtTime(localVolume, time);
@@ -106,8 +135,13 @@ export const usePlayClicktrack = (
       );
     }
 
-    oscillator.start(time);
-    oscillator.stop(time + metronomeSoundLength);
+    //oscillator.start(time);
+    //oscillator.stop(time + metronomeSoundLength);
+
+    //whiteNoiseSource.start(time);
+
+    clickSource.start(time);
+
   };
 
   /**
