940de9e18b41886bfb3af0b23107186fe2a33fb2
notjack <89240346+nootjack@users.noreply.github.com>
2025-11-09 14:46:10 -0800

    fix color issue on title

--- src/components/clicktrack/Title.tsx
+++ src/components/clicktrack/Title.tsx
@@ -14,7 +14,7 @@ export const Title = ({
   updateClicktrackData,
 }: ITitleButtons & ITitleInfo) => {
   return (
-    <div className="flex w-full items-center bg-zinc-200 px-4 py-2 dark:bg-zinc-900 md:justify-center md:bg-inherit md:py-8">
+    <div className="flex w-full items-center bg-zinc-200 px-4 py-2 dark:bg-zinc-900 md:justify-center md:bg-white md:py-8 md:dark:bg-black">
       <div className="flex w-full items-center justify-between md:justify-center md:px-6">
         <TitleInfo {...{ clicktrack }} />
         <div className="my-3 hidden h-px w-full bg-gradient-to-r from-transparent via-zinc-600/50 to-transparent sm:mx-6 sm:h-10 sm:w-px sm:bg-gradient-to-b md:block" />
