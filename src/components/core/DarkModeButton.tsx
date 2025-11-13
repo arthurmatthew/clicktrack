7a667687560ebd521b736b9d95c6a67fb73eee35
notjack <89240346+nootjack@users.noreply.github.com>
2025-11-12 22:03:46 -0800

    update accessibility

--- src/components/core/DarkModeButton.tsx
+++ src/components/core/DarkModeButton.tsx
@@ -5,7 +5,7 @@ export const DarkModeButton = ({ className }: IButton) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme}>
+    <button aria-label="Dark Mode" onClick={toggleTheme}>
       <i
         className={`${theme === 'dark' ? 'bi-moon' : 'bi-sun'} ${className}`}
       />
