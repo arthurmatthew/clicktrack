68c6f7862b0b0ded3687585cb73f0fe228b43c1d
notjack <89240346+nootjack@users.noreply.github.com>
2025-11-13 21:42:27 -0800

    attempt mobile resize fix

--- src/pages/app/+Layout.tsx
+++ src/pages/app/+Layout.tsx
@@ -6,7 +6,10 @@ import { LoadingScreen } from '../../components/core/LoadingScreen';
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="flex h-screen flex-col bg-white bg-size-[40px_40px] dark:bg-black">
+      <div
+        className="flex h-screen flex-col bg-white bg-size-[40px_40px] dark:bg-black"
+        style={{ height: '100dvh' }}
+      >
         <AppHeader />
         <main className="relative flex min-h-0 flex-auto flex-col">
           <Suspense fallback={<LoadingScreen />}>{children}</Suspense>
