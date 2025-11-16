7e10abb88517480cba8f981c29e355aca28fc844
notjack <89240346+nootjack@users.noreply.github.com>
2025-11-15 22:13:14 -0800

    update layout

--- src/components/clicktrack/SectionControls.tsx
+++ src/components/clicktrack/SectionControls.tsx
@@ -20,39 +20,34 @@ export const SectionControls = ({
   const nextId = sequence[sectionIndex + 1]?.id;
 
   return (
-    <div className="flex flex-col overflow-hidden rounded-sm">
-      <p className="col-span-2 text-lg">
-        Editing section{' '}
-        <span className="roboto">
-          <span className="rounded-md bg-zinc-200 px-1 dark:bg-zinc-900">
-            {sectionIndex + 1}
-          </span>
-          /
-          <span className="rounded-md bg-zinc-200 px-1 dark:bg-zinc-900">
-            {total}
-          </span>
+    <div className="flex justify-between gap-2 overflow-hidden">
+      <button
+        onClick={() => {
+          if (previousId) setSelectedId(previousId);
+        }}
+        disabled={previousId === undefined}
+        className="flex-1 rounded-sm bg-zinc-300 p-4 py-3 disabled:opacity-50 dark:bg-zinc-900"
+      >
+        Previous Section
+      </button>
+      <p className="roboto flex items-center text-2xl">
+        <span className="rounded-md bg-zinc-200 px-1 dark:bg-zinc-900">
+          {sectionIndex + 1}
+        </span>
+        <span className="mx-1 text-sm">/</span>
+        <span className="rounded-md bg-zinc-200 px-1 dark:bg-zinc-900">
+          {total}
         </span>
       </p>
-      <div className="grid grid-cols-2 gap-3">
-        <button
-          onClick={() => {
-            if (previousId) setSelectedId(previousId);
-          }}
-          disabled={previousId === undefined}
-          className="rounded-sm bg-zinc-300 p-4 py-3 duration-100 ease-out disabled:opacity-50 dark:bg-zinc-900"
-        >
-          Previous Section
-        </button>
-        <button
-          onClick={() => {
-            if (nextId) setSelectedId(nextId);
-          }}
-          disabled={nextId === undefined}
-          className="rounded-sm bg-zinc-300 p-4 py-3 duration-100 ease-out disabled:opacity-50 dark:bg-zinc-900"
-        >
-          Next Section
-        </button>
-      </div>
+      <button
+        onClick={() => {
+          if (nextId) setSelectedId(nextId);
+        }}
+        disabled={nextId === undefined}
+        className="flex-1 rounded-sm bg-zinc-300 p-4 py-3 disabled:opacity-50 dark:bg-zinc-900"
+      >
+        Next Section
+      </button>
     </div>
   );
 };
